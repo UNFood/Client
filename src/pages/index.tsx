0847aab59a3f90f6d92d446d06b2532a3d036e25
jubuenor <jubuenor@unal.edu.co>
2023-09-07 17:23:02 -0500

    extra: header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <div className="h-100 w-100 text-center">
-        <Header></Header>
-      </div>
+      <Header></Header>
     </>
   );
 }
