15b05e0f0c4ce519695b5bc57ea2302215e54f0f
jubuenor <jubuenor@unal.edu.co>
2023-09-15 12:35:36 -0500

    responsive

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -12,55 +12,54 @@ function LandingPage() {
             <h1 className="mt-5">UNFood</h1>
             <p>Comprar en la Nacho nunca fue tan facil . . .</p>
           </div>
-          <div className="d-flex justify-content-around mt-5 ">
-            <div
-              className={`${styles.landingbtn} rounded rounded-4 d-flex justify-content-evenly align-items-center cursor-pointer`}
-            >
-              <Image
-                src="/images/tienda.png"
-                alt="tienda"
-                width={120}
-                height={62}
-              ></Image>
-              CHAZA
-            </div>
-            <div className="vr text-light opacity-100"></div>
-            <div
-              className={`${styles.landingbtn} rounded rounded-4 d-flex justify-content-evenly align-items-center `}
-            >
-              CLIENTE
-              <Image
-                src="/images/donut.png"
-                alt="donut"
-                width={90}
-                height={87}
-              ></Image>
-            </div>
-          </div>
+          <Row className="mt-5 ">
+            <Col className="mb-3">
+              <div
+                className={`${styles.landingbtn} rounded rounded-4 d-flex justify-content-evenly align-items-center`}
+              >
+                <Image
+                  src="/images/tienda.png"
+                  alt="tienda"
+                  width={116}
+                  height={60}
+                ></Image>
+                CHAZA
+              </div>
+            </Col>
+            <Col className="mb-3">
+              <div
+                className={`${styles.landingbtn} float-end rounded rounded-4 d-flex justify-content-evenly align-items-center `}
+              >
+                CLIENTE
+                <Image
+                  src="/images/donut.png"
+                  alt="donut"
+                  width={87}
+                  height={85}
+                ></Image>
+              </div>
+            </Col>
+          </Row>
         </div>
       </section>
       <section className={`${styles.aboutus} p-5`}>
         <Row className="gx-0">
-          <Col>
+          <Col md={12} lg={6}>
             <h1 className="mt-5">¿Quienes somos?</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque gravida nulla nec libero tincidunt, sit amet
-              ultricies nulla posuere. Aenean porttitor sagittis vulputate.
-              Nulla blandit justo in elit venenatis gravida. Morbi pretium non
-              sem ut pretium. Aenean sodales quam a ex sodales, sed imperdiet
-              nisi porta. Sed erat ligula, luctus in viverra blandit, iaculis ut
-              orci. Duis feugiat mi eleifend, rhoncus mi in, consequat mi. Sed
-              id commodo risus.
+              Somos el soporte tanto para consumidores como chaceros en busca de
+              reducir los tiempos de atención presencial en las chazas. En esta
+              plataforma los clientes podrán realizar sus pedidos a las chazas
+              con anticipación y con ello, los chaceros tendrán el espacio
+              suficiente para la preparación de estos productos. Con esto
+              buscamos reducir la alta demanda en horas pico y los tiempos de
+              atención de manera significativa.
             </p>
           </Col>
-          <Col className="text-center m-auto">
-            <Image
-              src="/images/logoUNFood_3.png"
-              width={457}
-              height={279}
-              alt="logo"
-            ></Image>
+          <Col md={12} lg={6} className="text-center m-auto">
+            <div className={`${styles.img_container} `}>
+              <Image src="/images/logoUNFood_3.png" alt="logo" fill></Image>
+            </div>
           </Col>
         </Row>
       </section>
