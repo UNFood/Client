fbe563fe8c844b2ad6ddb7013b2036c19095fe7b
jubuenor <jubuenor@unal.edu.co>
2023-09-16 12:26:08 -0500

    counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 import styles from "../styles/home.module.css";
 
 function Counter() {
@@ -8,16 +8,23 @@ function Counter() {
       className={`${styles.counter} d-flex justify-content-center text-light p-2`}
     >
       <div>
-        <p className="text-light">
-          Mas de <span className="fs-1">0</span> Chazas
+        <Image src="/favicon.ico" alt="tienda" width={60} height={42}></Image>
+        <p className="fs-3 text-light ms-3 me-3">
+          Más de <span className="fs-1">0</span> Chazas
         </p>
       </div>
 
-      <div className="vr"></div>
+      <div className="vr opacity-100"></div>
       <div>
-        <p className="text-light">
-          {" "}
-          <span className="fs-1">0</span> Productos{" "}
+        <Image
+          className="ms-3"
+          src="/favicon.ico"
+          alt="dona"
+          width={60}
+          height={42}
+        ></Image>
+        <p className="fs-3 text-light ms-3">
+          Más de <span className="fs-1">0</span> Productos{" "}
         </p>
       </div>
     </div>
