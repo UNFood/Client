a5a0e0c36decc982bfee92e3cdda8d71ab6ee1eb
jubuenor <jubuenor@unal.edu.co>
2023-09-20 21:46:28 -0500

    products

---
+++ src/components/Client/Products.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+import styles from "../../styles/products.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { Card, Row, Col, Breadcrumb } from "react-bootstrap";
+
+const products = [
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+  {
+    name: "Empanada de pollo y champiñon",
+    description: "Empanada rellena de pollo desmechado, arroz y champiñones.",
+    price: "$2.800",
+    category: "Comida rapida",
+    image: "/images/empanada.png",
+    stock: 10,
+    total_sales: 0,
+  },
+];
+function Products() {
+  const renderProducts = products.map((product, index) => {
+    return (
+      <Col sm={6} md={4} xl={3} className="mb-5" key={index}>
+        <Card className={`${styles.product_card} m-auto`}>
+          <Image
+            src={product.image}
+            alt="emapanada"
+            width={260}
+            height={179}
+          ></Image>
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+            <h1>{product.price}</h1>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div className={`${styles.products}`}>
+      <div className="mb-5 mt-3">
+        <Breadcrumb>
+          <li className="breadcrumb-item">
+            <Link href="/client/home"> UNFood</Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link href="/client/products"> Productos</Link>
+          </li>
+        </Breadcrumb>
+      </div>
+      <Row className="gx-0">{renderProducts}</Row>
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/pages/client/products.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import Products from "../../components/Client/Products";
+
+function products() {
+  return (
+    <>
+      <Header></Header>
+      <Products></Products>
+    </>
+  );
+}
+
+export default products;
