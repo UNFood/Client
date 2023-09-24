ac874d7c8e49f1445917d4e063844fd2e8a494bf
jubuenor <jubuenor@unal.edu.co>
2023-09-23 19:26:43 -0500

    image from Nextjs

--- src/components/Chazaregister.tsx
+++ src/components/Chazaregister.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import styles from '../styles/register.module.css';
+import styles from "../styles/register.module.css";
 import { Form, Button } from "react-bootstrap";
-
+import Image from "next/image";
 
 function Chazaregister() {
   const [validated, setValidated] = useState(false);
@@ -27,103 +27,102 @@ function Chazaregister() {
   };
 
   return (
-    <div className={styles['form-container']}>
-    <>
-
-    <img
-        src="images/chazafondo.PNG"
-        alt="Background"
-        className={styles['background-image']}
-      />
-          
-      <div className={styles['form-box']}>
-        
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
-        className="w-10 text-center m-auto"
-      >
+    <div className={styles["form-container"]}>
+      <>
         <img
-                  src="/images/logoUNFood.png"
-                  alt="logoUNFood"
-                  height={100}
-                ></img>
-        <h1>Registro de Chaza</h1>
-        
-        <Form.Group className="mb-3">
-          <Form.Control
-            required
-            type="text"
-            placeholder="Nombre de usuario"
-            onChange={(e) => setUsername(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Nombre de usuario no valido
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="mb-3 ">
-          <Form.Control
-            required
-            type="text"
-            placeholder="Nombre de Chaza"
-            onChange={(e) => setChazaName(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Nombre no valido
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="mb-3 ">
-          <Form.Control
-            required
-            type="text"
-            placeholder="Tipo de Chaza"
-            onChange={(e) => setPhone(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Tipo de chaza no valido
-          </Form.Control.Feedback>
-        </Form.Group>
+          src="images/chazafondo.PNG"
+          alt="Background"
+          className={styles["background-image"]}
+        />
 
-        <Form.Group className="mb-3 ">
-          <Form.Control
-            required
-            type="Direccion"
-            placeholder="Telefono"
-            onChange={(e) => setDireccion(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Direccion no valido
-          </Form.Control.Feedback>
-        </Form.Group>
+        <div className={styles["form-box"]}>
+          <Form
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+            className="w-10 text-center m-auto"
+          >
+            <Image
+              src="/images/logoUNFood.png"
+              alt="logoUNFood"
+              width={327}
+              height={200}
+            ></Image>
+            <h1>Registro de Chaza</h1>
 
-        <Form.Group className="mb-3 ">
-          <Form.Control
-            required
-            type="text"
-            placeholder="Direccion"
-            onChange={(e) => setChazatipo(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Contraseña no valida
-          </Form.Control.Feedback>
-        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Nombre de usuario"
+                onChange={(e) => setUsername(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Nombre de usuario no valido
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3 ">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Nombre de Chaza"
+                onChange={(e) => setChazaName(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Nombre no valido
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3 ">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Tipo de Chaza"
+                onChange={(e) => setPhone(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Tipo de chaza no valido
+              </Form.Control.Feedback>
+            </Form.Group>
 
-        <Form.Group className="mb-3 ">
-          <Form.Control
-            required
-            type="text"
-            placeholder="Descripcion"
-            onChange={(e) => setDescription(e.target.value)}
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Descripcion no valida
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button type="submit">Registrarse</Button>
-      </Form>
-      </div>
-    </>
+            <Form.Group className="mb-3 ">
+              <Form.Control
+                required
+                type="Direccion"
+                placeholder="Telefono"
+                onChange={(e) => setDireccion(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Direccion no valido
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3 ">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Direccion"
+                onChange={(e) => setChazatipo(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Contraseña no valida
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3 ">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Descripcion"
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+              <Form.Control.Feedback type="invalid">
+                Descripcion no valida
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit">Registrarse</Button>
+          </Form>
+        </div>
+      </>
     </div>
   );
 }
