861fcc6f14012e1af093b89a483184046da63988
jubuenor <jubuenor@unal.edu.co>
2023-10-11 09:46:21 -0500

    header client chaza

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import styles from "@/styles/navbar.module.css";
 import { BiMap } from "react-icons/bi";
 import { FaUserAlt, FaShoppingCart } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
+import { BiStoreAlt, BiPackage } from "react-icons/bi";
 
 // @ToDo hacer header
 
@@ -24,29 +25,31 @@ function Header() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse
             id="navbarScroll"
-            className="justify-content-evenly h-25"
+            className="justify-content-between"
           >
-            <Form className="d-flex w-md-50 w-sm-80    ms-3">
+            <Form className="d-flex m-auto text-center">
               <Form.Control
                 type="search"
                 placeholder="Busca tus productos favoritos"
                 aria-label="Search"
+                className="m-auto"
               />
               <Button variant="light">
                 <BsSearch size={20} />
               </Button>
             </Form>
-
-            <Nav className="ms-3" style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav className="ms-3" navbarScroll>
               <Nav.Link href="#action1" className="text-center">
                 <BiMap size={25} />
                 <span className="ms-2">Ubicacion</span>
               </Nav.Link>
               <Nav.Link href="products" className="text-center me-3">
-                Productos
+                <BiPackage size={25} />
+                <span> Productos</span>
               </Nav.Link>
               <Nav.Link href="chazas" className="text-center me-3">
-                Chazas
+                <BiStoreAlt size={25} />
+                <span>Chazas</span>
               </Nav.Link>
               <Nav.Link href="#" className="text-center">
                 <FaShoppingCart size={25} />
