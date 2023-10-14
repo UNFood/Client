ced5ee95b2ec301b5d32bfa03c15b618a41395a0
jubuenor <jubuenor@unal.edu.co>
2023-10-14 16:28:10 -0500

    lsita chaza fixes

--- src/pages/client/chazas.tsx
+++ src/pages/client/chazas.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from "react";
-import {useQuery} from "react-query";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import Loading from "@/components/Loading";
 import ListaChaza from "@/components/Chaza/ListaChaza";
 import { getChazas } from "../api/chaza";
-function home() {
+import Header from "@/components/Header";
 
+function home() {
   const {
     status,
     error,
@@ -12,16 +13,18 @@ function home() {
   } = useQuery({
     queryKey: ["getChazas"],
     queryFn: () => getChazas(),
-  
   });
-
-  
 
   if (status === "loading") return <Loading></Loading>;
   if (status === "error") return <h1>{JSON.stringify(error)}</h1>;
-  if (chazas === null) return <h1>Error</h1>;
-  if (chazas === undefined) return <h1>Error</h1>;
-  return <ListaChaza chazas={chazas.data}></ListaChaza>;
+  if (chazas === null || chazas === undefined) return <h1>Error</h1>;
+
+  return (
+    <>
+      <Header></Header>
+      <ListaChaza chazas={chazas.data}></ListaChaza>
+    </>
+  );
 }
 
 export default home;
