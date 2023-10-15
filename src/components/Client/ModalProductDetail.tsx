62d8903fdaca1b334fb345d57cc10443d2cc0986
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-10-15 14:25:13 -0500

    Mejora de modalproductdetail colores

--- src/components/Client/ModalProductDetail.tsx
+++ src/components/Client/ModalProductDetail.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
 import Modal from "react-bootstrap/Modal";
+import { FaTrash } from 'react-icons/fa';
 import { Product } from "@/types/product";
 import categoriasChaza from "@/utils/categoriesChaza";
 import { Button } from "react-bootstrap";
@@ -21,7 +22,38 @@ function ModalProductDetail({
     reviews: "250 Reviews",
     deliveryTime: "30-45 mins"
   };
+  
   const categoryName = categoriasChaza[product?.category];
+  
+  const handleAddToCart = (count) => {
+    // Implement your logic to add the product to the cart
+    console.log("Agregado al carro",count);
+  };
+  
+  const [count, setCount] = useState(1);
+  
+  const handleAddToCartAndBuy = (count) => {
+    // Implement your logic to add the product to the cart
+    console.log("Agregado al carro y comprando",count);
+  };
+  
+
+  // Function to handle counter increment
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  // Function to handle counter decrement
+  const handleDecrement = () => {
+    if(count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+    // Function to reset counter
+    const handleReset = () => {
+      setCount(0);
+    };
   return (
     <Modal show={show} onHide={handleClose} size="lg" centered>
       <Modal.Header closeButton>
@@ -41,10 +73,20 @@ function ModalProductDetail({
         <p><strong>Unidades Disponibles:</strong> {product?.stock}</p>
         <p><strong>Tiempo estimado de entrega:</strong> {staticInfo.deliveryTime}</p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={handleClose}>Close</Button>
-      </Modal.Footer>
+      <Modal.Footer style={{ justifyContent: 'flex-start' }}>
+  <Button style={{ backgroundColor: '#A63C6D', borderColor: '#A63C6D' }} size="sm" onClick={handleReset}>
+    <FaTrash />
+  </Button>
+  <Button style={{ backgroundColor: '#A63C6D', borderColor: '#A63C6D' }} size="sm" onClick={handleDecrement}>-</Button>
+  <span style={{ margin: '0 10px' }}>{count}</span>
+  <Button style={{ backgroundColor: '#A63C6D', borderColor: '#A63C6D' }} size="sm" onClick={handleIncrement}>+</Button>
+  <Button style={{ backgroundColor: '#A63C6D', borderColor: '#A63C6D' }} size="sm" onClick={() => handleAddToCart(count)}>Agregar y seguir comprando</Button>
+  <Button style={{ backgroundColor: '#550A2D', borderColor: '#550A2D' }} size="sm" onClick={() => handleAddToCartAndBuy(count)}>Agregar y comprar</Button>
+</Modal.Footer>
+
     </Modal>
+
+//style={{ backgroundColor: "#550A2D" }}
   );
 }
 
