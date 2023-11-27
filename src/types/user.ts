e4cceec1ef49df610990a75e62ec812e71ea7607
jubuenor <jubuenor@unal.edu.co>
2023-11-26 21:39:18 -0500

    update user

--- src/types/user.ts
+++ src/types/user.ts
@@ -9,9 +9,21 @@ export interface signupData {
   lastName: string;
   email: string;
   password: string;
+  phone: string;
 }
 
 export interface tokenData {
   id: string;
   username: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  address: string;
+  phone: string;
 }
